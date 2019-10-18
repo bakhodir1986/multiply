fc04f89c5e6decc55828f8ed602de0dc65d2232f
bakhodirradjabov <bakhodir.radjabov@olsoft.biz>
2019-10-18 17:32:02 +0500

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function multiply(first, second) {
-  // your solution
+    // your solution
+    console.log(first)
+    console.log(second)
+    var n_first = BigInt(first)
+    var n_second = BigInt(second)
+    var multiply_num = n_first * n_second
+    console.log(multiply_num)
+    return String(multiply_num)
 }
